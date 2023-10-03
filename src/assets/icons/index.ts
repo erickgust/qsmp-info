9f38c29031418c7407f58582eba2e58aa29288f2
Erick Gustavo <erick.stazs@gmail.com>
2023-10-03 14:12:08 -0300

    feat(icons): add social media icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,13 @@
+import reddit from './reddit.svg'
+import twitter from './twitter.svg'
+import tiktok from './tiktok.svg'
+import instagram from './instagram.svg'
+import facebook from './facebook.svg'
+
+export const icons = new Map([
+  ['reddit', reddit],
+  ['twitter', twitter],
+  ['tiktok', tiktok],
+  ['instagram', instagram],
+  ['facebook', facebook],
+])
