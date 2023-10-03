3fc6e0be803185c795dff35c79355680a4ef98ad
Erick Gustavo <erick.stazs@gmail.com>
2023-10-03 14:59:21 -0300

    style: improve Footer layout and styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,24 +4,24 @@ import { links } from './data'
 export function Footer () {
   return (
     <footer className='bg-[#FAE39B]'>
-      <div className='max-w-7xl flex px-2 py-4'>
-        <div className='flex-1'>
-          <h1 className='font-pixel text-2xl uppercase'>
+      <div className='max-w-7xl flex px-4 py-4 mx-auto pt-20 items-end'>
+        <div className='flex-[4] text-[#132B3F]'>
+          <h1 className='font-pixel text-2xl uppercase mb-8'>
             Official <br />
             QSMP accounts
           </h1>
 
-          <p>
+          <p className='leading-7 block max-w-md'>
             Nunc egestas urna ac orci vel sed erat. Sodales vel netus facilisi donec donec quis.
             Risus adipiscing mauris cum non est enim a. Quis commodo sit sed lectus ac est.
 
-            <br />
-
-            Amet lobortis eu malesuada penatibus aliquet nisi nec.
+            <span className='mt-3 block'>
+              Amet lobortis eu malesuada penatibus aliquet nisi nec.
+            </span>
           </p>
         </div>
 
-        <aside className='flex-1'>
+        <aside className='flex-[5]'>
           <ul className='flex gap-5 gap-y-6 flex-wrap justify-end'>
             {links.map((link) => (
               <SocialMediaItem
