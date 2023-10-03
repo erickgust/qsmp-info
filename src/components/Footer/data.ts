cdf0468a1321f308ee30c8d3910dcbd312ba1c21
Erick Gustavo <erick.stazs@gmail.com>
2023-10-03 14:13:46 -0300

    feat(Footer): Add social media links and icons

---
+++ src/components/Footer/data.ts
@@ -0,0 +1,62 @@
+export const links = [
+  {
+    name: 'QSMP Instagram',
+    url: 'https://www.instagram.com/qsmpreal/',
+    icon: 'instagram',
+  },
+  {
+    name: 'QSMP Facebook',
+    url: 'https://www.facebook.com/qsmpreal/',
+    icon: 'facebook',
+  },
+  {
+    name: 'R/QSMP',
+    url: 'https://www.reddit.com/r/QSMP/',
+    icon: 'reddit',
+  },
+  {
+    name: 'QSMPReal Reddit',
+    url: 'https://www.reddit.com/user/QSMPReal/',
+    icon: 'reddit',
+  },
+  {
+    name: 'Quackity Studios',
+    url: 'https://twitter.com/QuackityStudios',
+    icon: 'twitter',
+  },
+  {
+    name: 'Quackity Merch',
+    url: 'https://twitter.com/QuackityMerchh',
+    icon: 'twitter',
+  },
+  {
+    name: 'QSMP Global',
+    url: 'https://twitter.com/QSMPGlobal',
+    icon: 'twitter',
+  },
+  {
+    name: 'QSMP English',
+    url: 'https://twitter.com/QsmpEN',
+    icon: 'twitter',
+  },
+  {
+    name: 'QSMP Spanish',
+    url: 'https://twitter.com/QsmpES',
+    icon: 'twitter',
+  },
+  {
+    name: 'QSMP Portuguese',
+    url: 'https://twitter.com/QsmpPOR',
+    icon: 'twitter',
+  },
+  {
+    name: 'QSMP French',
+    url: 'https://twitter.com/QsmpFRA',
+    icon: 'twitter',
+  },
+  {
+    name: 'QSMP Official',
+    url: 'https://www.tiktok.com/@qsmpenglish',
+    icon: 'tiktok',
+  },
+] as const
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,43 @@
+import { Button } from '../Button'
+import { links } from './data'
+
+import { icons } from '@/assets/icons'
+
 export function Footer () {
   return (
-    <footer>
-      <div>
-        <h1 className='font-pixel text-2xl'>Official QSMP accounts</h1>
+    <footer className='bg-[#FAE39B]'>
+      <div className='max-w-7xl flex px-2 py-4'>
+        <div className='flex-1'>
+          <h1 className='font-pixel text-2xl uppercase'>
+            Official <br />
+            QSMP accounts
+          </h1>
 
-        <p>
-          Nunc egestas urna ac orci vel sed erat. Sodales vel netus facilisi donec donec quis.
-          Risus adipiscing mauris cum non est enim a. Quis commodo sit sed lectus ac est.
+          <p>
+            Nunc egestas urna ac orci vel sed erat. Sodales vel netus facilisi donec donec quis.
+            Risus adipiscing mauris cum non est enim a. Quis commodo sit sed lectus ac est.
 
-          <br />
+            <br />
 
-          Amet lobortis eu mausuada penatibus aliquet nisi nec.
-        </p>
+            Amet lobortis eu malesuada penatibus aliquet nisi nec.
+          </p>
+        </div>
+
+        <aside className='flex-1'>
+          <ul className='flex gap-5 gap-y-6 flex-wrap justify-end'>
+            {links.map((link) => (
+              <li key={link.url} className='inline-block'>
+                <a href={link.url} target='_blank' rel='noopener noreferrer'>
+                  <Button size='small' variant='white'>
+                    <img src={icons.get(link.icon)} alt='' />
+                    {link.name}
+                  </Button>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </aside>
       </div>
-
     </footer>
   )
 }
