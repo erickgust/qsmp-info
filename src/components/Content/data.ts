1be0a7ae059feddcc6e2973f60adae3cb46b6113
Erick Gustavo <erick.stazs@gmail.com>
2023-10-04 00:47:10 -0300

    ref: add twitchName in member data

--- src/components/Content/data.ts
+++ src/components/Content/data.ts
@@ -1,7 +1,7 @@
 export type Member = {
-  id: string
   name: string
   isLive: boolean
+  twitchName: string
   avatarURL: string
   liveChannelURL: string
   country: {
@@ -12,9 +12,9 @@ export type Member = {
 
 export const members: Member[] = [
   {
-    id: 'bagherajones',
-    name: 'Baghera Jones',
     isLive: false,
+    name: 'Baghera Jones',
+    twitchName: 'bagherajones',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/5f161f8d-ef6a-4276-b6ba-4639df412aab-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/bagherajones',
     country: {
@@ -23,9 +23,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'bagi',
-    name: 'Bagi',
     isLive: false,
+    name: 'Bagi',
+    twitchName: 'bagi',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/762a31e8-9829-4957-8073-dd85d67ad576-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/bagi',
     country: {
@@ -34,9 +34,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'ironmouse',
-    name: 'ironmouse',
     isLive: false,
+    name: 'ironmouse',
+    twitchName: 'ironmouse',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/c2aca19b-b318-4398-99a5-fb6a597536fd-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/ironmouse',
     country: {
@@ -45,9 +45,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'BadBoyHalo',
-    name: 'BadBoyHalo',
     isLive: false,
+    name: 'BadBoyHalo',
+    twitchName: 'BadBoyHalo',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/c2277621-ce23-4f16-b75e-1d12ef23b617-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/badboyhalo',
     country: {
@@ -56,9 +56,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'FitMC',
-    name: 'FitMC',
     isLive: false,
+    name: 'FitMC',
+    twitchName: 'FitMC',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/9ff631ba-0389-4cd7-88fa-239a9969819f-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/fitmc',
     country: {
@@ -67,9 +67,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'Foolish',
-    name: 'Foolish',
     isLive: false,
+    name: 'Foolish',
+    twitchName: 'Foolish_Gamers',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/66e83125-195e-4431-bde8-89ac425a9038-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/Foolish_Gamers',
     country: {
@@ -78,9 +78,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'jaidenanimations',
-    name: 'Jaiden',
     isLive: false,
+    name: 'Jaiden',
+    twitchName: 'jaidenanimations',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/jaidenanimations-profile_image-3e0e81c44b77b57a-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/jaidenanimations',
     country: {
@@ -89,9 +89,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'Quackity',
-    name: 'Quackity',
     isLive: false,
+    name: 'Quackity',
+    twitchName: 'Quackity',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/823b1a32-eb4e-425a-b1e6-48913ed58f1b-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/quackity',
     country: {
@@ -100,9 +100,20 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'tubbo',
-    name: 'Tubbo',
     isLive: false,
+    name: 'Felps',
+    twitchName: 'felps',
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/50942827-74fa-407b-9edc-bc3827e47f9f-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/felps',
+    country: {
+      ISOCode: 'BR',
+      name: 'Brazil',
+    },
+  },
+  {
+    isLive: false,
+    name: 'Tubbo',
+    twitchName: 'tubbo',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/905af69a-4fd8-42c7-b842-bf4ee4d51b3b-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/tubbo',
     country: {
@@ -111,9 +122,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'roier',
-    name: 'Roier',
     isLive: false,
+    name: 'Roier',
+    twitchName: 'roier',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/84138061-a22c-40ff-8ca4-fc41e70ff513-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/roier',
     country: {
@@ -122,9 +133,9 @@ export const members: Member[] = [
     },
   },
   {
-    id: 'peqitw',
-    name: 'Pac',
     isLive: false,
+    name: 'Pac',
+    twitchName: 'peqitw',
     avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/267d5dd6-5710-4687-8464-858348cc1255-profile_image-300x300.png',
     liveChannelURL: 'https://www.twitch.tv/peqitw',
     country: {
