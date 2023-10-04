92383348d50486401903d8d8c71ce56894017480
Erick Gustavo <erick.stazs@gmail.com>
2023-10-03 23:59:34 -0300

    feat: add member data to Content component

---
+++ src/components/Content/data.ts
@@ -0,0 +1,135 @@
+export type Member = {
+  id: string
+  name: string
+  isLive: boolean
+  avatarURL: string
+  liveChannelURL: string
+  country: {
+    ISOCode: string
+    name: string
+  }
+}
+
+export const members: Member[] = [
+  {
+    id: 'bagherajones',
+    name: 'Baghera Jones',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/5f161f8d-ef6a-4276-b6ba-4639df412aab-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/bagherajones',
+    country: {
+      ISOCode: 'CH',
+      name: 'Switzerland',
+    },
+  },
+  {
+    id: 'bagi',
+    name: 'Bagi',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/762a31e8-9829-4957-8073-dd85d67ad576-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/bagi',
+    country: {
+      ISOCode: 'BR',
+      name: 'Brazil',
+    },
+  },
+  {
+    id: 'ironmouse',
+    name: 'ironmouse',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/c2aca19b-b318-4398-99a5-fb6a597536fd-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/ironmouse',
+    country: {
+      ISOCode: 'PR',
+      name: 'Puerto Rico',
+    },
+  },
+  {
+    id: 'BadBoyHalo',
+    name: 'BadBoyHalo',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/c2277621-ce23-4f16-b75e-1d12ef23b617-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/badboyhalo',
+    country: {
+      ISOCode: 'US',
+      name: 'United States',
+    },
+  },
+  {
+    id: 'FitMC',
+    name: 'FitMC',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/9ff631ba-0389-4cd7-88fa-239a9969819f-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/fitmc',
+    country: {
+      ISOCode: 'US',
+      name: 'United States',
+    },
+  },
+  {
+    id: 'Foolish',
+    name: 'Foolish',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/66e83125-195e-4431-bde8-89ac425a9038-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/Foolish_Gamers',
+    country: {
+      ISOCode: 'US',
+      name: 'United States',
+    },
+  },
+  {
+    id: 'jaidenanimations',
+    name: 'Jaiden',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/jaidenanimations-profile_image-3e0e81c44b77b57a-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/jaidenanimations',
+    country: {
+      ISOCode: 'US',
+      name: 'United States',
+    },
+  },
+  {
+    id: 'Quackity',
+    name: 'Quackity',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/823b1a32-eb4e-425a-b1e6-48913ed58f1b-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/quackity',
+    country: {
+      ISOCode: 'MX',
+      name: 'Mexico',
+    },
+  },
+  {
+    id: 'tubbo',
+    name: 'Tubbo',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/905af69a-4fd8-42c7-b842-bf4ee4d51b3b-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/tubbo',
+    country: {
+      ISOCode: 'GB',
+      name: 'United Kingdom',
+    },
+  },
+  {
+    id: 'roier',
+    name: 'Roier',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/84138061-a22c-40ff-8ca4-fc41e70ff513-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/roier',
+    country: {
+      ISOCode: 'MX',
+      name: 'Mexico',
+    },
+  },
+  {
+    id: 'peqitw',
+    name: 'Pac',
+    isLive: false,
+    avatarURL: 'https://static-cdn.jtvnw.net/jtv_user_pictures/267d5dd6-5710-4687-8464-858348cc1255-profile_image-300x300.png',
+    liveChannelURL: 'https://www.twitch.tv/peqitw',
+    country: {
+      ISOCode: 'BR',
+      name: 'Brazil',
+    },
+  },
+]
