61db6ecbce098840b22c6207d380d159417cb6af
Erick Gustavo <erick.stazs@gmail.com>
2023-10-03 23:55:43 -0300

    feat: add Member component

---
+++ src/components/Content/Member.tsx
@@ -0,0 +1,60 @@
+import { ReactComponent as ExternalLinkIcon } from '@/assets/icons/external-link.svg'
+import { ReactComponent as Menu } from '@/assets/icons/menu.svg'
+import { Button } from '../Button'
+
+type MemberProps = {
+  name: string
+  image: string
+  flag: {
+    url: string
+    alt: string
+  }
+  liveChannelURL: string
+  isLive: boolean
+}
+
+export function Member ({ name, image, flag, liveChannelURL, isLive }: MemberProps) {
+  return (
+    <article className='w-88 min-w-max bg-neutral-100 relative'>
+      {isLive && (
+        <span className='text-white bg-red-600 px-3 py-1 text-xl absolute left-6 top-7'>
+          LIVE
+        </span>
+      )}
+
+      <img
+        src={image}
+        alt={`Profile picture of ${name}`}
+        className='w-full h-88 object-cover'
+      />
+
+      <div className='p-6'>
+        <div className='flex justify-between mb-9 items-center'>
+          <strong className='font-normal text-2xl'>
+            {name}
+          </strong>
+
+          <img
+            src={flag.url}
+            alt={`${flag.alt} flag`}
+            width='36'
+            height='27'
+          />
+        </div>
+
+        <div className='flex justify-between'>
+          <Button variant='dark' type='icon'>
+            <Menu />
+          </Button>
+
+          <a href={liveChannelURL} target='_blank' rel='noopener noreferrer'>
+            <Button>
+              <span className='pt-[0.125rem]'>Live Channel</span>
+              <ExternalLinkIcon />
+            </Button>
+          </a>
+        </div>
+      </div>
+    </article>
+  )
+}
